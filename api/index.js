716d1412645031d3cc0c536134aa452af00d6160
hemanth-8660 <chiruvellahemanth@gmail.com>
2025-07-07 09:08:33 +0530

    added log

--- api/index.js
+++ api/index.js
@@ -3,28 +3,28 @@ const express = require('express');
 const app = express();
 const logger = require('log4js').getLogger('INDEX');
 const Mongo = require('../utils/mongo');
-
+const routes = require('../routes/index');
 const cors = require('cors');
 app.use(cors());
 
-const routes = require('../routes/index');
 const config = process.env;
 const mongo = new Mongo(config.MONGO_URL);
 
-
-mongo.connect().then(() => {
-    logger.info('Connected to mongo...');
-}).catch((err) => {
-    logger.error(err.toString());
+app.use((req, res, next) => {
+    mongo.connect().then(() => {
+        logger.info('Connected to mongo...');
+        next();
+    }).catch((err) => {
+        logger.error(err.toString());
+    })
 })
 
 app.use(express.json({limit: '20mb'}));
 
-routes(app);
 app.get('/', (req, res) => {
     return res.send('Express on Vercel')
 })
-
+routes(app);
 app.listen(config.PORT, () => {
     console.log(`Server ready on port ${config.PORT}.`)
 })
